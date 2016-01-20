da470590222bf7901768993061cf4c1ca53923d2
Sylwia Grudowska <s.grudowska@gmail.com>
2016-01-20 10:54:38 +0100

    Clean up code, change the way of access to memory and visibility. Remove arduino remote option

--- Robik.ino
+++ Robik.ino
@@ -1,6 +1,6 @@
 #include "IRremote.h"
 #include "PCF8574.h"
-#include <Wire.h>
+#include "Wire.h"
 
 #define debug false
 
@@ -12,9 +12,10 @@
 #define IN3 7
 #define IN4 6
 
+// blink led when push the remote button
 #define WHITE_LED 3
 
-// ir receiver digital pin
+// ir received signals from remote
 #define IR_RECEIVER 11
 
 // tv remote hex signals
@@ -22,30 +23,24 @@ const unsigned long tv_up = 0xE0E006F9;
 const unsigned long tv_down = 0xE0E08679;
 const unsigned long tv_right = 0xE0E0A659;
 const unsigned long tv_left = 0xE0E046B9;
-// stop motors
-const unsigned long stop_engines = 0xE0E016E9;
-// change speed
-const unsigned long faster = 0xE0E0E01F;
-const unsigned long slower = 0xE0E0D02F;
+// signal to stop motors
+const unsigned long tv_stop_engines = 0xE0E016E9;
+// signals to change speed of motors
+const unsigned long tv_faster = 0xE0E0E01F;
+const unsigned long tv_slower = 0xE0E0D02F;
 
 // current motors speed
-int motors_speed = 155;
-
-// arduino remote hex signals
-const unsigned long arduino_up = 0xFF629D;
-const unsigned long arduino_down = 0xFF02FD;
-const unsigned long arduino_left = 0xFF22DD;
-const unsigned long arduino_right = 0xFFC23D;
+static int motors_speed = 155;
 
 // current signal code
-unsigned long signal_code = 0x00000000;
+static unsigned long signal_code = 0x00000000;
 
 // HCSR501 calibration time in sec.
 // TODO change to 15
-int calibration_time_HCSR501 = 3;
+static int calibration_time_HCSR501 = 3;
 
-IRrecv irrecv(IR_RECEIVER);
-decode_results ir_results;
+static IRrecv irrecv(IR_RECEIVER);
+static decode_results ir_results;
 
 // struct describes HCSR501 motion sensors
 struct motion_sensor {
@@ -67,19 +62,19 @@ struct motion_sensor {
 };
 
 // initalize motion sensors
-struct motion_sensor motion_nr0 = motion_sensor(0, 12);
+static struct motion_sensor motion_nr0 = motion_sensor(0, 12);
 // struct motion_sensor motion_nr1(1, 0);
-struct motion_sensor motion_nr2(2, 4);
-struct motion_sensor motion_nr3(3, 13);
+static struct motion_sensor motion_nr2(2, 4);
+static struct motion_sensor motion_nr3(3, 13);
 
-void start_motors(int, int, int, int, int);
-void check_IR_signal();
-void blink_white_led();
-void stop_motors();
-void faster_motors();
-void slower_motors();
-void calibrate_motion_sensor(int);
-bool motion_detected(int);
+static void start_motors(int, int, int, int, int);
+static void check_IR_signal();
+static void blink_white_led();
+static void stop_motors();
+static void faster_motors();
+static void slower_motors();
+static void calibrate_motion_sensor(int);
+static bool motion_detected(int);
 
 void setup() {
   // motors pin mode
@@ -134,58 +129,59 @@ void loop() {
   // check status of the of sensors and stop
   // when return to the first one again
   while (element->next->ID_sensor != 3) {
-    if (motion_detected((*element).pin_sensor) &&
-    motion_detected((*(*element).next.pin_sensor)) {
+    // if (motion_detected((*element).pin_sensor) &&
+    // motion_detected((*(*element).next).pin_sensor)
+    {
 
-    Serial.println((*(*element).next).ID_sensor);
+      Serial.println((*(*element).next).ID_sensor);
     }
     element = element->next;
   }
 }
 
-void check_IR_signal() {
+static void check_IR_signal() {
 
   signal_code = ir_results.value;
 
   while (signal_code) {
-    if (signal_code == arduino_up || signal_code == tv_up) {
+    if (signal_code == tv_up) {
       blink_white_led();
       // Serial.println("up");
       start_motors(LOW, HIGH, LOW, HIGH, motors_speed);
-    } else if (signal_code == arduino_down || signal_code == tv_down) {
+    } else if (signal_code == tv_down) {
       blink_white_led();
       // Serial.println("down");
       start_motors(HIGH, LOW, HIGH, LOW, motors_speed);
-    } else if (signal_code == arduino_left || signal_code == tv_left) {
+    } else if (signal_code == tv_left) {
       blink_white_led();
       // Serial.println("left");
       start_motors(HIGH, LOW, LOW, HIGH, motors_speed);
-    } else if (signal_code == arduino_right || signal_code == tv_right) {
+    } else if (signal_code == tv_right) {
       blink_white_led();
       // Serial.println("right");
       start_motors(LOW, HIGH, HIGH, LOW, motors_speed);
-    } else if (signal_code == stop_engines) {
+    } else if (signal_code == tv_stop_engines) {
       blink_white_led();
       stop_motors();
-    } else if (signal_code == faster) {
+    } else if (signal_code == tv_faster) {
       faster_motors();
-    } else if (signal_code == slower) {
+    } else if (signal_code == tv_slower) {
       slower_motors();
     }
     signal_code = 0x0000000;
   }
 }
 
-void blink_white_led() {
+static void blink_white_led() {
   digitalWrite(3, HIGH);
   delay(100);
   digitalWrite(3, LOW);
   delay(100);
 }
 
-void start_motors(int directionL1, int directionL2,
-                  int directionR1, int directionR2,
-                  int speedLR) {
+static void start_motors(int directionL1, int directionL2,
+                         int directionR1, int directionR2,
+                         int speedLR) {
   digitalWrite(IN1, directionL1);
   digitalWrite(IN2, directionL2);
   // range 0~255
@@ -195,7 +191,7 @@ void start_motors(int directionL1, int directionL2,
   analogWrite(RIGHT_ENGINE_SPEED, speedLR);
 }
 
-void stop_motors() {
+static void stop_motors() {
   // turn off motors
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
@@ -203,7 +199,7 @@ void stop_motors() {
   digitalWrite(IN4, LOW);
 }
 
-void faster_motors() {
+static void faster_motors() {
   // usable range 35 - 255;
   // change of 20
   if ((motors_speed + 20) > 255) {
@@ -217,7 +213,7 @@ void faster_motors() {
   }
 }
 
-void slower_motors() {
+static void slower_motors() {
   // usable range 35 - 255
   // change of 20
   if ((motors_speed - 20) < 35) {
@@ -232,7 +228,7 @@ void slower_motors() {
 }
 
 // calibrate HCSR501 sensor
-void calibrate_motion_sensor(int sensor_pin) {
+static void calibrate_motion_sensor(int sensor_pin) {
   pinMode(sensor_pin, INPUT);
   digitalWrite(sensor_pin, LOW);
   for (int i = 0; i < calibration_time_HCSR501; ++i) {
@@ -240,7 +236,7 @@ void calibrate_motion_sensor(int sensor_pin) {
   }
 }
 
-bool motion_detected(int sensor_pin) {
+static bool motion_detected(int sensor_pin) {
   int val = digitalRead(sensor_pin);
   if (val == LOW) {
     return false;

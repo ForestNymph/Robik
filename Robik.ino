98ebfc04f64b0dca89e3f0ed9bbecd22246b9afd
Sylwia Grudowska <s.grudowska@gmail.com>
2016-02-05 12:02:12 +0100

    Photoresistors added. New mode: moth_turtle. Change algorithm behavior when motion is detected. Refactored.

--- Robik.ino
+++ Robik.ino
@@ -6,13 +6,19 @@
 
 #define debug false
 
-// Robic can operate in three different modes:
+// Robic can operate in a four different modes:
+#define remote_turtle 0
 // [0] A robot controlled by the user with remote control
+#define friendly_turtle 1
 // [1] Self-propelled robot moving through the source of motion
 // capable to verify the distance from objects
+#define shy_turtle 2
 // [2] A runaway robot, always keeps a specified distance
-// To change modes set a number [0, 1, 2] value for 'robot_mode'
-#define robot_mode 1
+#define moth_turtle 3
+// [3] Robic becomes a moth and looks for a light source
+
+// To change mode set a name mode for 'robot_mode'
+#define robot_mode friendly_turtle
 
 static void (*start_robic)();
 
@@ -39,7 +45,7 @@ static PCF8574 expander;
 #define IN3 7 // right
 #define IN4 6 // right
 
-static int motors_speed = 100;
+static int motors_speed = 120;
 
 // struct describes full configuration of engines
 // like a speed and motors direction
@@ -67,6 +73,7 @@ struct motors_config {
 };
 
 static void run_motors(motors_config*);
+static void run_motors_with_distance(motors_config*, int);
 static void boost_speed();
 static void reduce_speed();
 
@@ -82,12 +89,10 @@ static struct motors_config m_stop(LOW, LOW, LOW, LOW, HIGH);
 ///////// RED/YELLOW/GREEN LED //////////////////////////
 
 #define RED_LED 13
-#define YELLOW_LED A4
+#define YELLOW_LED A2
 
-#define GREEN_LED_0 A0
+#define GREEN_LED_3 A0
 #define GREEN_LED_1 A1
-#define GREEN_LED_2 A2
-#define GREEN_LED_3 A3
 
 static void blink_red_led();
 static void green_led_on(int);
@@ -130,8 +135,6 @@ static unsigned long signal_code = 0x00000000;
 // In this case interrupts are not needed
 
 #define ENCODER_WHEEL 4
-
-static void turn(motors_config*, int);
 
 ///////// MOTION SENSOR HCSR501 (expander) //////////////
 
@@ -194,18 +197,31 @@ static decode_results ir_results;
 static void detect_IR_signal();
 static void check_IR_signal();
 
-///////// DISTANCE SENSOR HC-SR04 (expander)/////////////
+///////// DISTANCE SENSOR HC-SR04 ///////////////////////
 
 #define HC_SR04_TRIGGER 10
 #define HC_SR04_ECHO 12
 
-// minimum distance from object: 80 cm
-static long min_distance = 80;
+// minimum distance from object: 20 cm
+static long min_distance = 20;
 
-static bool detect_min_distance();
+static bool detected_min_distance();
 static long convert_microsec_to_centimeters();
 
 static void detect_distance();
+
+///////// PHOTORESISTOR GL5616 //////////////////////////
+
+#define PHOTORESISTOR_LEFT A4
+#define PHOTORESISTOR_RIGHT A5
+#define PHOTORESISTOR_CENTER A3
+
+// sensor value needed to trigger an action
+static int left_tolerance = 800;
+static int right_tolerance = 800;
+static int center_tolerance = 800;
+
+static void detect_light();
 
 ///////// MICRO SERVO TOWER PRO SG90 ////////////////////
 
@@ -225,8 +241,6 @@ void setup() {
   // led mode
   pinMode(RED_LED, OUTPUT);
   digitalWrite(RED_LED, LOW);
-  pinMode(YELLOW_LED, OUTPUT);
-  digitalWrite(YELLOW_LED, LOW);
 
   // motors pin mode
   // don't need set pin mode as OUTPUT before calling analogWrite()
@@ -242,29 +256,30 @@ void setup() {
   pinMode(HC_SR04_TRIGGER, OUTPUT);
   pinMode(HC_SR04_ECHO, INPUT);
 
-  if (robot_mode == 0) {
+  if (robot_mode == remote_turtle) {
 
     // start the IR receiver
     irrecv.enableIRIn();
     start_robic = &detect_IR_signal;
 
-  } else if (robot_mode == 1) {
+  } else if (robot_mode == friendly_turtle) {
+
     // set expander on 0x20 adress, all bits on low state (pins to GND)
     // 0 1 0 0 A2 A1 A0 - (Ax) can be modify
     // lowest adress 0x20, highest 0x27 (32-39)
     // 8 different adresses can be set
     expander.begin(0x20);
 
+    pinMode(YELLOW_LED, OUTPUT);
+    analogWrite(YELLOW_LED, 0);
 
     // internal pull up resistors ON (20-50kOhm)
     // needed to eliminate interference on pin
     pinMode(ENCODER_WHEEL, INPUT_PULLUP);
     digitalWrite(ENCODER_WHEEL, HIGH);
 
-    // mode for led connected to analog pin
-    pinMode(GREEN_LED_0, OUTPUT);
+    // mode for led 1 and 3 connected to analog pin
     pinMode(GREEN_LED_1, OUTPUT);
-    pinMode(GREEN_LED_2, OUTPUT);
     pinMode(GREEN_LED_3, OUTPUT);
 
     // HCSR501 calibration
@@ -280,13 +295,19 @@ void setup() {
     // servo.attach(SERVO);
 
     start_robic = &detect_motion;
-  } else if (robot_mode == 3) {
+
+  } else if (robot_mode == shy_turtle) {
+
     start_robic = &detect_distance;
+
+  } else if (robot_mode == moth_turtle) {
+
+    start_robic = &detect_light;
   }
 
   if (debug) {
     Serial.begin(9600);
-    Serial.println(F("Works!"));
+    Serial.println(F("Debug ON"));
   }
 }
 
@@ -294,8 +315,34 @@ void loop() {
   (*start_robic)();
 }
 
+static void detect_light() {
+
+  int light_from_left = analogRead(PHOTORESISTOR_LEFT);
+  int light_from_right = analogRead(PHOTORESISTOR_RIGHT);
+  int light_from_center = analogRead(PHOTORESISTOR_CENTER);
+
+  if (debug) {
+    Serial.print(F("Left: "));
+    Serial.println(light_from_left);
+    Serial.print(F("Right: "));
+    Serial.println(light_from_right);
+    Serial.print(F("Center: "));
+    Serial.println(light_from_center);
+  }
+
+  if (light_from_center > center_tolerance) {
+    run_motors_with_distance(&m_forward, 8);
+  }
+  if (light_from_right > right_tolerance) {
+    run_motors_with_distance(&m_right, 8);
+  }
+  if (light_from_left > left_tolerance) {
+    run_motors_with_distance(&m_left, 8);
+  }
+}
+
 static void detect_distance() {
-  if (detect_min_distance()) {
+  if (detected_min_distance()) {
     run_motors(&m_backward);
   } else {
     run_motors(&m_forward);
@@ -350,8 +397,8 @@ static void detect_motion() {
     // if motion detected by two of the sensors
     if (motion_detected((*element).pin_sensor) &&
         motion_detected((*(*element).next).pin_sensor)) {
-      green_led_on((*element).ID_sensor);
-      green_led_on((*(*element).next).ID_sensor);
+      // green_led_on((*element).ID_sensor);
+      // green_led_on((*(*element).next).ID_sensor);
       turn_to_motion_direction((*element).ID_sensor, true);
       if (debug) {
         Serial.println(F("Motion detected both sides!"));
@@ -372,14 +419,14 @@ static void detect_motion() {
 // if motion is detected return HIGH state
 static bool motion_detected(int sensor_pin) {
   int val = expander.digitalRead(sensor_pin);
-  if (val == LOW) {
-    return false;
-  } else {
+  if (val == HIGH) {
     return true;
+  } else {
+    return false;
   }
 }
 
-static bool detect_min_distance() {
+static bool detected_min_distance() {
   long duration, distance = 0;
   digitalWrite(HC_SR04_TRIGGER, LOW);
   delayMicroseconds(2);
@@ -389,10 +436,10 @@ static bool detect_min_distance() {
   duration = pulseIn(HC_SR04_ECHO, HIGH);
   distance = convert_micorsec_to_centimeters(duration);
 
-  if (distance > min_distance) {
-    return true;
+  if (min_distance < distance) {
+    return false;
   }
-  return false;
+  return true;
 }
 
 static long convert_micorsec_to_centimeters(long microseconds) {
@@ -400,27 +447,6 @@ static long convert_micorsec_to_centimeters(long microseconds) {
   // The ping travels out and back, so to find the distance of the
   // object we take half of the distance travelled.
   return microseconds / 29 / 2;
-}
-
-// number_encoder_pulses
-// 8 pulses it is a full rotation of the wheel
-// it is independent from speed
-static void turn(motors_config* conf,
-                 int number_encoder_pulses) {
-  int encoder_turn_count = 0;
-  byte new_state, old_state = digitalRead(ENCODER_WHEEL);
-  run_motors(conf);
-  while (true) {
-    if (new_state != old_state) {
-      ++encoder_turn_count;
-      old_state = new_state;
-      if (encoder_turn_count >= number_encoder_pulses) {
-        break;
-      }
-    }
-    new_state = digitalRead(ENCODER_WHEEL);
-  }
-  run_motors(&m_stop);
 }
 
 static void run_motors(motors_config* conf) {
@@ -435,6 +461,88 @@ static void run_motors(motors_config* conf) {
 
   analogWrite(LEFT_ENGINE_SPEED, (*conf).speed1);
   analogWrite(RIGHT_ENGINE_SPEED, (*conf).speed2);
+}
+
+// number_encoder_pulses
+// 8 pulses it is a full rotation of the wheel
+// it is independent from speed
+static void run_motors_with_distance(motors_config* conf,
+                                     int number_encoder_pulses) {
+  int encoder_turn_count = 0;
+  byte new_state, old_state = digitalRead(ENCODER_WHEEL);
+  run_motors(conf);
+  while (!detected_min_distance()) {
+    if (new_state != old_state) {
+      ++encoder_turn_count;
+      old_state = new_state;
+      if (encoder_turn_count >= number_encoder_pulses) {
+        break;
+      }
+    }
+    new_state = digitalRead(ENCODER_WHEEL);
+  }
+  // if min distance detected move backward by 1 second
+  if (detected_min_distance()) {
+    run_motors(&m_backward);
+    // delay because motion sensors
+    // need time to get LOW state
+    delay(1000);
+  }
+  run_motors(&m_stop);
+}
+
+static void turn_to_motion_direction(int pin, bool between) {
+  if (pin == 0) {
+    if (between) { // between 0 and 1
+      run_motors_with_distance(&m_right, 14);
+    } else {
+      // move detected in front of Robik
+      // don't turn around just go forward
+    }
+  } else if (pin == 1) {
+    if (between) { // between 1 and 2
+      run_motors_with_distance(&m_right, 16);
+    } else {
+      run_motors_with_distance(&m_right, 12);
+    }
+  } else if (pin == 2) {
+    if (between) { // between 2 and 3
+      run_motors_with_distance(&m_left, 14);
+    } else {
+      run_motors_with_distance(&m_right, 16);
+    }
+  } else if (pin == 3) {
+    if (between) { // between 3 and 0
+      run_motors_with_distance(&m_left, 14);
+    } else {
+      run_motors_with_distance(&m_left, 12);
+    }
+  }
+  // after turn always go forward specified distance
+  // and then stop
+  run_motors_with_distance(&m_forward, 16);
+  run_motors(&m_stop);
+}
+
+// calibrate HCSR501 sensor
+// set INPUT mode for digtal pins on expander
+static void calibrate_motion_sensors() {
+  digitalWrite(YELLOW_LED, 255);
+  expander.pinMode(MOTION_0, INPUT);
+  expander.pinMode(MOTION_1, INPUT);
+  expander.pinMode(MOTION_2, INPUT);
+  expander.pinMode(MOTION_3, INPUT);
+  // all pins disable the internal pull down on the input pin
+  // http://forum.arduino.cc/index.php?topic=5313.0
+  // Only disable internal pullup but this is not pull down
+  expander.digitalWrite(MOTION_0, LOW);
+  expander.digitalWrite(MOTION_1, LOW);
+  expander.digitalWrite(MOTION_2, LOW);
+  expander.digitalWrite(MOTION_3, LOW);
+  for (int i = 0; i < calibration_time_HCSR501; ++i) {
+    delay(1000);
+  }
+  digitalWrite(YELLOW_LED, 0);
 }
 
 static void boost_speed() {
@@ -457,56 +565,6 @@ static void reduce_speed() {
   }
 }
 
-// calibrate HCSR501 sensor
-// set INPUT mode for digtal pins on expander
-static void calibrate_motion_sensors() {
-  digitalWrite(YELLOW_LED, HIGH);
-  expander.pinMode(MOTION_0, INPUT);
-  expander.pinMode(MOTION_1, INPUT);
-  expander.pinMode(MOTION_2, INPUT);
-  expander.pinMode(MOTION_3, INPUT);
-  // all pins disable the internal pull down on the input pin
-  // http://forum.arduino.cc/index.php?topic=5313.0
-  // Only disable internal pullup but this is not pull down
-  expander.write(LOW);
-  for (int i = 0; i < calibration_time_HCSR501; ++i) {
-    delay(1000);
-  }
-  digitalWrite(YELLOW_LED, LOW);
-}
-
-static void turn_to_motion_direction(int pin, bool between) {
-  if (pin == 0) {
-    if (between) { // between 0 and 1
-      turn(&m_right, 14);
-    } else {
-      // move detected in front of Robik
-      // don't turn around just go forward
-    }
-  } else if (pin == 1) {
-    if (between) { // between 1 and 2
-      turn(&m_right, 16);
-    } else {
-      turn(&m_right, 12);
-    }
-  } else if (pin == 2) {
-    if (between) { // between 2 and 3
-      turn(&m_left, 14);
-    } else {
-      turn(&m_right, 16);
-    }
-  } else if (pin == 3) {
-    if (between) { // between 3 and 0
-      turn(&m_left, 14);
-    } else {
-      turn(&m_left, 12);
-    }
-  }
-  //run_motors(&m_forward);
-  //while (!detect_min_distance()) {}
-  run_motors(&m_stop);
-}
-
 static void blink_red_led() {
   digitalWrite(RED_LED, HIGH);
   delay(100);
@@ -515,22 +573,22 @@ static void blink_red_led() {
 }
 
 static void green_led_on(int pin) {
-  if (pin == 0) {
-    digitalWrite(GREEN_LED_0, HIGH);
+  if (pin == 3) {
+    digitalWrite(GREEN_LED_3, HIGH);
   } else if (pin == 1) {
     digitalWrite(GREEN_LED_1, HIGH);
-  } else if (pin == 2) {
+  } /*else if (pin == 2) {
     digitalWrite(GREEN_LED_2, HIGH);
   } else if (pin == 3) {
-    digitalWrite(GREEN_LED_3, HIGH);
-  }
+    digitalWrite/(GREEN_LED_3, HIGH);
+  }*/
 }
 
 static void green_led_off() {
-  digitalWrite(GREEN_LED_0, LOW);
   digitalWrite(GREEN_LED_1, LOW);
-  digitalWrite(GREEN_LED_2, LOW);
   digitalWrite(GREEN_LED_3, LOW);
+  // digitalWrite(GREEN_LED_2, LOW);
+  // digitalWrite(GREEN_LED_0, LOW);
 }
 
 //static void run_servo() {
